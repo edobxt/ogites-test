52f8825a3947fdd9756b7c0edd38cd0dd2d78ba2
kanjo <kanjo.school@gmail.com>
2020-11-11 02:58:32 -0400

    Just beautify

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ô'GÎTES</title>
-        <link rel="stylesheet" type="text/css" href="style.css"/>
-        <link rel="stylesheet" href="scss/searchbox.scss"/>
-        <link rel="stylesheet" href="scss/slideshow.scss"/>
-        <link rel="stylesheet" href="scss/scrolldown.scss"/>
-    </head>
-    <header>
-        <h1>Ô'GÎTES</h1>
-        <nav id="top-menu">
-            <ul>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">Présentation</a></li>
-                <li><a href="#">À propos</a></li>
-                <li><a href="login-system/register.php">Connexion</a></li>
-            </ul>
-        </nav>
-    </header>
-    
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ô'GÎTES</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" href="scss/searchbox.scss" />
+    <link rel="stylesheet" href="scss/slideshow.scss" />
+    <link rel="stylesheet" href="scss/scrolldown.scss" />
+</head>
+<header>
+    <h1><a href="#">Ô'GÎTES</a></h1>
+    <nav id="top-menu">
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Présentation</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="login-system/register.php">Connexion</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php  require_once 'header.php'; ?>
 
-    <div class="main-container">
-        <div class="describ">
-            <h1>Trouvez votre lieu favori parmi une selection des meilleurs gîtes de Guadeloupe</h1>
-        </div>
-        
-        <div class="searchbox">
-            <?php include 'assets/input.php'; ?>
-        </div>
-
-        <br/>
-
-        <div class="slideshow">
-            <?php include 'assets/slideshow.php'; ?>
-        </div>
-
-        <br/><br/><br/><br/><br/><br/>
-        
-        <div class="scrolldown">
-            <?php include 'assets/scrolldown.php'; ?>
-        </div>
+<div class="main-container">
+    <div class="describ">
+        <h1>Trouvez votre lieu favori parmi une selection des meilleurs gîtes de Guadeloupe</h1>
     </div>
+
+    <div class="searchbox">
+        <?php include 'assets/input.php'; ?>
+    </div>
+
+    <br />
+
+    <div class="slideshow">
+        <?php include 'assets/slideshow.php'; ?>
+    </div>
+
+    <br /><br /><br /><br /><br /><br />
+
+    <div class="scrolldown">
+        <?php include 'assets/scrolldown.php'; ?>
+    </div>
+</div>
 
 <?php require_once 'footer.php'; ?>
